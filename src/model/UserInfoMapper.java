834902476fb6c080f3a876bb590ce3baa57422e6
Diki Faturrohman <118197085+DikiFaturrohman@users.noreply.github.com>
2025-01-10 17:46:38 +0700

    Membuat Dashboard Admin

--- src/model/UserInfoMapper.java
+++ src/model/UserInfoMapper.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.List;
 import org.apache.ibatis.annotations.*;
 
 public interface UserInfoMapper {
@@ -20,12 +21,21 @@ public interface UserInfoMapper {
     @Update("UPDATE user_info SET username = #{username}, phone = #{phone}, address = #{address}, profile_picture = #{profilePicturePath} WHERE id = #{id}")
     int updateUserProfile(UserInfo user);
 
-    @Select("SELECT * FROM user_info WHERE username = #{username}")
+    @Select("SELECT id, username, password, email, phone, address, profile_picture, role FROM user_info WHERE username = #{username}")
     UserInfo selectUserByUsername(String username);
+
 
     @Select("SELECT * FROM user_info WHERE username = #{username} AND password = #{password}")
     UserInfo selectUserByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 
     @Update("UPDATE user_info SET password = #{password} WHERE id = #{id}")
     int updatePassword(@Param("id") int id, @Param("password") String password);
+    
+        // Add this method to fetch all users
+    @Select("SELECT id, username, email, address, role FROM user_info")
+    List<UserInfo> selectAllUsers();  // This will retrieve all users
+    
+    @Delete("DELETE FROM user_info WHERE id = #{id}")
+    void deleteUserById(int id);
+
 }
